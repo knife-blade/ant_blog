ab66fd925f65f8b60cb7fbf52ba36406189aa152
water <2716552863@qq.com>
2021-10-31 00:21:36 +0800

    修改部分数据库数据

--- ant_backend/business.sql
+++ ant_backend/business.sql
@@ -36,10 +36,24 @@ CREATE TABLE `t_user`
     INDEX             `INDEX_USERNAME`(`user_name`)
 ) ENGINE = InnoDB;
 
-INSERT INTO `t_blog` VALUES (1, 1, '博客1的标题', '博客1的摘要', '博客1的内容', 0, '2021-01-23 11:33:36', '2021-01-23 11:33:36');
-INSERT INTO `t_blog` VALUES (2, 1, '博客2的标题', '博客2的摘要', '博客2的内容', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
-
 -- 账号：knife，密码：222333    密码会加盐，两次md5之后进行保存
 INSERT INTO `t_user` VALUES (1, 'knife', '刀刃',
-        'https://image-1300566513.cos.ap-guangzhou.myqcloud.com/upload/images/5a9f48118166308daba8b6da7e466aab.jpg',
-        NULL, 'e4b8c8e43f8fabbe08d5aa67d58068ac', 'sCPKiMOoEl2ecPsfFhClcg==', 0, NULL, NULL, NULL);
+                             'https://image-1300566513.cos.ap-guangzhou.myqcloud.com/upload/images/5a9f48118166308daba8b6da7e466aab.jpg',
+                             NULL, 'e4b8c8e43f8fabbe08d5aa67d58068ac', 'sCPKiMOoEl2ecPsfFhClcg==', 0, NULL, NULL, NULL);
+
+INSERT INTO `t_blog` VALUES (1, 1, 'Java中枚举的用法', '本文介绍Java的枚举类的使用', '枚举一般用于定义一些常量', 0, '2021-01-23 11:33:36', '2021-01-23 11:33:36');
+INSERT INTO `t_blog` VALUES (2, 1, 'Java中泛型的用法', '本文介绍Java的泛型的使用', '一些框架的源码中经常看到泛型，学习泛型可以帮助我们更好的理解框架，也可以提高编程水平', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (3, 1, 'Java的HashMap的原理', '本文介绍Java的HashMap的原理',  'HashMap实际上是一个大的数组，key是数组的下标，value是数组的值', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (4, 1, 'Java中BigDecimal的用法', '本文介绍Java的BigDecimal的使用', 'BigDecimal处理数字很精确，而且可以表示大于16位的数', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (5, 1, 'Java中反射的用法', '本文介绍Java的反射的使用', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (6, 1, 'Java的ArrayList保证线程安全的方法', '本文介绍如何保证ArrayList的线程安全', 'ArrayList不是线程安全的，有多种方法可以保证线程安全', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (7, 1, 'SpringBoot启动的流程', '本文介绍SpringBoot启动的流程', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (8, 1, 'ArrayList扩容原理', '本文介绍Java的ArrayList扩容的原理', 'ArrayList实例化后容量为0，往里放数据时才会分配容量', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (9, 1, 'Java中反射的用法', '本文介绍Java的反射的使用', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (10, 1, 'Java中反射的用法', '本文介绍Java的反射的使用', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (11, 1, 'Java中反射的用法', '本文介绍Java的反射的使用', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (12, 1, 'Java中反射的用法', '本文介绍Java的反射的使用', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (13, 1, 'Java中反射的用法', '本文介绍Java的反射的使用', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+INSERT INTO `t_blog` VALUES (14, 1, 'Java中反射的用法', '本文介绍Java的反射的使用', '反射一般用于通过Class获得实例、调用方法等', 0, '2021-01-28 23:37:37', '2021-01-28 23:37:37');
+
+
