2c72983e5cc30608c068ce6a5a2ec6844874916a
water <2716552863@qq.com>
2021-11-21 00:47:38 +0800

    不重要的修改

--- ant_frontend/src/views/Login.vue
+++ ant_frontend/src/views/Login.vue
@@ -37,11 +37,11 @@ export default {
       },
       rules: {
         userName: [
-          { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+          {required: true, message: '请输入用户名', trigger: 'blur'},
+          {min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur'}
         ],
         password: [
-          { required: true, message: '请选择密码', trigger: 'change' }
+          {required: true, message: '请选择密码', trigger: 'change'}
         ]
       }
     };
@@ -50,18 +50,19 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          const _this = this
-          this.$axios.post('/login', this.ruleForm).then(res => {
-            console.log(res.data)
-            const jwt = res.headers['Authorization']
-            const userInfo = res.data.data
-            // 把数据共享出去
-            _this.$store.commit("SET_TOKEN", jwt)
-            _this.$store.commit("SET_USERINFO", userInfo)
-            // 获取
-            console.log(_this.$store.getters.getUser)
-            _this.$router.push("/blogs")
-          })
+          const _this = this;
+          this.$axios.post('/login', this.ruleForm)
+              .then(res => {
+                console.log(res.data);
+                const jwt = res.headers['Authorization'];
+                const userInfo = res.data.data;
+                // 把数据共享出去
+                _this.$store.commit("SET_TOKEN", jwt);
+                _this.$store.commit("SET_USERINFO", userInfo);
+                // 获取
+                console.log(_this.$store.getters.getUser);
+                _this.$router.push("/blogs");
+              })
         } else {
           console.log('error submit!!');
           return false;
@@ -82,32 +83,39 @@ export default {
   text-align: center;
   line-height: 60px;
 }
+
 .el-aside {
   background-color: #D3DCE6;
   color: #333;
   text-align: center;
   line-height: 200px;
 }
+
 .el-main {
   /*background-color: #E9EEF3;*/
   color: #333;
   text-align: center;
   line-height: 160px;
 }
+
 body > .el-container {
   margin-bottom: 40px;
 }
+
 .el-container:nth-child(5) .el-aside,
 .el-container:nth-child(6) .el-aside {
   line-height: 260px;
 }
+
 .el-container:nth-child(7) .el-aside {
   line-height: 320px;
 }
+
 .logo {
   height: 60%;
   margin-top: 10px;
 }
+
 .demo-ruleForm {
   max-width: 500px;
   margin: 0 auto;
