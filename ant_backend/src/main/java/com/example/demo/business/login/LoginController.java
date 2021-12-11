5d1905260d88f98da6de5b622816bb3bb36020ee
water <2716552863@qq.com>
2021-12-11 23:13:11 +0800

    登录和注册放到同一个接口中

--- ant_backend/src/main/java/com/example/demo/business/login/LoginController.java
+++ ant_backend/src/main/java/com/example/demo/business/login/LoginController.java
@@ -7,10 +7,12 @@ import com.example.demo.common.constant.AuthConstant;
 import com.example.demo.common.entity.Result;
 import com.example.demo.common.exception.BusinessException;
 import com.example.demo.common.util.auth.JwtUtil;
+import com.example.demo.config.properties.UserProperty;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
@@ -21,28 +23,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
-@Api(tags = "登录")
+@Api(tags = "登录/注册")
 @RestController
 @RequestMapping("/login")
 public class LoginController {
     @Autowired
     private UserService userService;
 
-    // 默认账号密码：knife 222333
-    @ApiOperation("登录")
+    @Autowired
+    private UserProperty userProperty;
+
+    /**
+     * 若未注册则自动注册，若已注册则登录
+     */
+    @ApiOperation("登录/注册")
     @PostMapping
     public Result<UserVO> login(@RequestBody User user, HttpServletResponse response) {
         Assert.hasLength(user.getUserName(), "用户名不能为空");
         Assert.hasLength(user.getPassword(), "密码不能为空");
+
         User userFromDB = userService.lambdaQuery().eq(User::getUserName, user.getUserName()).one();
+        if (userFromDB == null) {
+            userFromDB = register(user.getUserName(), user.getPassword());
+        } else {
+            String calculatedPassword = new SimpleHash(AuthConstant.ALGORITHM_TYPE,
+                    user.getPassword(), userFromDB.getSalt(), AuthConstant.HASH_ITERATIONS).toString();
 
-        Assert.notNull(userFromDB, "用户不存在");
-
-        String calculatedPassword = new SimpleHash(AuthConstant.ALGORITHM_TYPE,
-                user.getPassword(), userFromDB.getSalt(), AuthConstant.HASH_ITERATIONS).toString();
-
-        if (!userFromDB.getPassword().equals(calculatedPassword)) {
-            throw new BusinessException("用户名或密码不正确");
+            if (!userFromDB.getPassword().equals(calculatedPassword)) {
+                throw new BusinessException("用户名或密码不正确");
+            }
         }
 
         String jwt = JwtUtil.createToken(userFromDB.getId().toString());
@@ -58,5 +67,19 @@ public class LoginController {
         userVO.setEmail(userFromDB.getEmail());
 
         return new Result<UserVO>().success().data(userVO);
+    }
+
+    private User register(String userName, String password) {
+        User user = new User();
+        user.setUserName(userName);
+        user.setNickName(userName);
+
+        String salt = new SecureRandomNumberGenerator().nextBytes().toString();
+        String calculatedPassword = new SimpleHash(AuthConstant.ALGORITHM_TYPE,
+                password, salt, AuthConstant.HASH_ITERATIONS).toString();
+        user.setPassword(calculatedPassword);
+        user.setAvatarUrl(userProperty.getDefaultAvatarUrl());
+        userService.save(user);
+        return user;
     }
 }
